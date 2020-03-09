071985d1223ee8dd6048b2129ccab4eb3d7ef761
kenji.komachi <rgby1308xazkk@gmail.com>
2020-03-09 12:42:17 +0000

    カラムとバリデーションの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_104157) do
+ActiveRecord::Schema.define(version: 2020_03_09_120535) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
