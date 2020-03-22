b21ae818c626df44e90d43e0c1df895188960d44
kenji.komachi <rgby1308xazkk@gmail.com>
2020-03-22 11:14:25 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  
   resources :tasks
   
-  get'toppage', to: 'toppages#index'
+  get 'signup', to: 'users#new'
+  resources :users, only: [:new, :create]
+  
+  get 'toppage', to: 'toppages#index'
 end
